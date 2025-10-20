9a5e41b83d2905d3a2b3a0f532e74305f82ef1c9
Rajneesh yadav <surajkumar304890@gmail.com>
2025-10-20 17:13:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ TOKEN_COUNTER = 0
 CYCLE_COUNT = 0
 LAST_MESSAGE_TIME = None
 START_TIME = datetime.now()
-RENDER_URL = "https://your-app-name.onrender.com"  # APNA RENDER URL DALDO
+RENDER_URL = "https://raj-testing 20 oct 25.onrender.com"  # APNA RENDER URL DALDO
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
